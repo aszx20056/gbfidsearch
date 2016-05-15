0ae579aae75659914fa04819cfee9e1db0b67787
aszx20056 <aszx20056@gmail.com>
2016-05-15 15:08:20 +0800

    first line
    first line

---
+++ gbf/Controllers/indexController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using Tweetinvi;
+using Tweetinvi.Core.Authentication;
+using Tweetinvi.Core.Enum;
+using Tweetinvi.Core.Parameters;
+using Tweetinvi.Json;
+using System.Text;
+using System.IO;
+using System.Net;
+namespace gbf.Controllers
+{
+    public class indexController : Controller
+    {
+        // GET: index
+        const string TWITTERAPPACCESSTOKEN = "";
+        const string TWITTERAPPACCESSTOKENSECRET = "";
+        const string TWITTERAPPAPIKEY = "";
+        const string TWITTERAPPAPISECRET = "";
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult datapage()
+        {
+            Auth.SetUserCredentials(TWITTERAPPAPIKEY, TWITTERAPPAPISECRET, TWITTERAPPACCESSTOKEN, TWITTERAPPACCESSTOKENSECRET);
+            string userJson = TwitterAccessor.ExecuteJsonGETQuery("https://api.twitter.com/1.1/search/tweets.json?q=参加者募集！参戦ID:&result_type=recent&count=20");
+            return Content(userJson);
+        }
+    }
+}
